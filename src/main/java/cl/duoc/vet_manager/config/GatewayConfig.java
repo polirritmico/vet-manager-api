62341667c2ab4f57840125281a45402cf43696dd
polirritmico <ebray187@gmail.com>
2026-05-27 15:26:26 -0400

    refactor(GatewayConfig): minor var names changes

--- src/main/java/cl/duoc/vet_manager/config/GatewayConfig.java
+++ src/main/java/cl/duoc/vet_manager/config/GatewayConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
     @Value("${app.service.appointments.base-url}")
-    private String appointmentsBaseUrl;
+    private String apptsBaseUrl;
 
     @Value("${app.service.pets.base-url}")
     private String petsBaseUrl;
@@ -26,7 +26,8 @@ public class GatewayConfig {
     @Bean
     public RouteLocator domainRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("appts_pt", req -> req.path("/api/v1/appointments/**").uri(appointmentsBaseUrl))
+                .route("appt_pt", req -> req.path("/api/v1/appointments/**").uri(apptsBaseUrl))
+                .route("clinic_pt", r -> r.path("/api/v1/clinical-records/**").uri(apptsBaseUrl))
                 .route("pets_passthrough", req -> req.path("/api/v1/pets/**").uri(petsBaseUrl))
                 .route("vets_passthrough", req -> req.path("/api/v1/vets/**").uri(vetsBaseUrl))
                 .build();
