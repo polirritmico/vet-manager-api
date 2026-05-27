64f85ed73e569528106ac30cb630971457476f70
polirritmico <ebray187@gmail.com>
2026-05-27 00:10:38 -0400

    feat(StatusController): refactor controller & add check status appointments

--- src/main/java/cl/duoc/vet_manager/controller/StatusController.java
+++ src/main/java/cl/duoc/vet_manager/controller/StatusController.java
@@ -6,20 +6,35 @@
  */
 package cl.duoc.vet_manager.controller;
 
+import cl.duoc.vet_manager.client.AppointmentsClient;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
 @Tag(name = "System", description = "Service monitoring & status")
 public class StatusController {
+    private final AppointmentsClient appointmentsClient;
 
-    @GetMapping("/api/v1/health")
+    @GetMapping("/health")
     @Operation(summary = "Check service health", description = "Checks the microservice avaliability")
     public ResponseEntity<Map<String, String>> checkHealth() {
         return ResponseEntity.ok(Map.of("status", "OK", "service", "VetManager"));
+    }
+
+    @GetMapping("/health/appointments")
+    @Operation(
+            summary = "Check the appointments-api health",
+            description = "Checks the Appointments-api microservice avaliability")
+    public Mono<Map> checkAppointmentsHealth() {
+        return appointmentsClient.getHealth();
     }
 }
