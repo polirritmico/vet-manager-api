ca258a82851c6f142107aa9e2485af9c1242c6fd
polirritmico <ebray187@gmail.com>
2026-05-27 15:36:13 -0400

    refactor(ApiClients): clean microservices WebClients

--- src/main/java/cl/duoc/vet_manager/client/AppointmentsClient.java
+++ src/main/java/cl/duoc/vet_manager/client/AppointmentsClient.java
@@ -6,17 +6,12 @@
  */
 package cl.duoc.vet_manager.client;
 
-import cl.duoc.vet_manager.dto.appointments.AppointmentResponse;
 import java.util.Map;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import reactor.core.publisher.Mono;
 
 public interface AppointmentsClient {
     public static final String base = "/api/v1/appointments";
-
-    @GetExchange(base + "/{id}")
-    Mono<AppointmentResponse> getAppointment(@PathVariable("id") Integer id);
 
     @GetExchange("/api/v1/health")
     Mono<Map> getHealth();
