83162a8cac57ac3407d9331fe68cb5bf8d725959
polirritmico <ebray187@gmail.com>
2026-05-27 00:08:36 -0400

    feat(security): disable default reactive user auth configuration

--- src/main/java/cl/duoc/vet_manager/VetManagerApplication.java
+++ src/main/java/cl/duoc/vet_manager/VetManagerApplication.java
@@ -8,9 +8,9 @@ package cl.duoc.vet_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
 public class VetManagerApplication {
 
     public static void main(String[] args) {
